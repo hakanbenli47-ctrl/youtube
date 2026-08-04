fafd718524a6d6e764e0b46d43d5cca5c25d9afd
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2026-08-04 23:08:10 +0300

    Yeni YouTube veri katmanını tek girişten dışa aktar

---
+++ lib/youtube-v2.ts
@@ -0,0 +1,3 @@
+export { createYouTubeAuthUrl, exchangeYouTubeCode, oauthClient } from "./youtube-core";
+export { syncYouTube } from "./youtube-sync";
+export { scanTrends } from "./youtube-trends";
