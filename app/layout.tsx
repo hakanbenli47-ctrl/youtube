acd397fa8ea9e495fa04e67dbbc33a373db00aa1
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2026-08-08 00:48:35 +0300

    Tema mobil yama dosyasını yükle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./studio-theme-v3.css";
+import "./theme-patch-v3.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
