87ea528267ef7b6787ac0d063f88963bf0d636d3
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2026-08-08 00:43:29 +0300

    Yeni Studio temasını etkinleştir

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "./youtube-theme.css";
+import "./studio-theme-v3.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
