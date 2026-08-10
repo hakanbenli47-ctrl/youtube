51175569b4d44895467af28686014ace357573a8
hakanbenli47-ctrl <hakanbenli47@gmail.com>
2026-08-11 00:16:36 +0300

    Hızlı YouTube bootstrap akışını dışa aktar

--- lib/youtube-v2.ts
+++ lib/youtube-v2.ts
@@ -1,3 +1,4 @@
 export { createYouTubeAuthUrl, exchangeYouTubeCode, oauthClient } from "./youtube-core";
+export { bootstrapPublicYouTubeState } from "./youtube-bootstrap";
 export { refreshPublicYouTubeStats, syncYouTube } from "./youtube-sync";
 export { scanTrends } from "./youtube-trends";
